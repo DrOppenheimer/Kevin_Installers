d53d2d23761143d3b8de1aa3408002a535a35528
Kevin Keegan <kevin.p.keegan@gmail.com>
2014-07-16 16:31:09 -0500

    apt-get clean & remove to clear up space for qiime

--- Install_AMETHST_compute_node.sh
+++ Install_AMETHST_compute_node.sh
@@ -54,10 +54,10 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 ### for qiime install later, uncomment the universe and multiverse repositories from /etc/apt/sources.list
 sed -e '/verse$/s/^#\{1,\}//' /etc/apt/sources.list > /etc/apt/sources.list.edit; mv /etc/apt/sources.list.edit /etc/apt/sources.list
 ### update and upgrade
+apt-get -y install build-essential
 apt-get -y update
 apt-get -y upgrade 
 ### install required packages
-apt-get -y install build-essential
 apt-get -y install python-dev libncurses5-dev libssl-dev libzmq-dev libgsl0-dev openjdk-6-jdk libxml2 libxslt1.1 libxslt1-dev ant git subversion zlib1g-dev libpng12-dev libfreetype6-dev mpich2 libreadline-dev gfortran unzip libmysqlclient18 libmysqlclient-dev ghc sqlite3 libsqlite3-dev libc6-i386 libbz2-dev libx11-dev libcairo2-dev libcurl4-openssl-dev libglu1-mesa-dev freeglut3-dev mesa-common-dev xorg openbox emacs r-cran-rgl xorg-dev libxml2-dev mongodb-server bzr make gcc mercurial python-qcli
 apt-get -y autoremove
 apt-get -y autoclean
